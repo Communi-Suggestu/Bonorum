6195fce0812c89440c8b923ae7c34769d8bf712d
Marc Hermans <marc.hermans@ldtteam.com>
2026-01-08 17:11:40 +0100

    Add support for metadata generation.

--- src/main/java/com/communi/suggestu/bonorum/ProjectPlugin.java
+++ src/main/java/com/communi/suggestu/bonorum/ProjectPlugin.java
@@ -9,6 +9,7 @@ import org.gradle.api.Project;
 import org.gradle.api.file.RegularFile;
 import org.gradle.api.plugins.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 
 import java.io.File;
 
@@ -26,6 +27,29 @@ public class ProjectPlugin implements Plugin<Project> {
 
         RunManager runs = project.getExtensions().getByType(RunManager.class);
 
+        project.getRepositories().exclusiveContent(exclusiveContentRepository -> {
+            exclusiveContentRepository.forRepositories(
+                project.getRepositories().maven(repository -> {
+                    repository.setName("Sinytra - Wiki Exporter");
+                    repository.setUrl("https://maven.sinytra.org");
+                })
+            );
+            exclusiveContentRepository.filter(filter-> {
+                filter.includeModule("org.sinytra", "wiki-exporter-neoforge");
+            });
+        });
+
+        project.getConfigurations().getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)
+            .getDependencies().addLater(extension.getSinytraExporterVersion()
+                .map("org.sinytra:wiki-exporter-neoforge:%s"::formatted)
+                .map(project.getDependencies()::create));
+
+        createExtractTextures(project, runs, extension);
+        createExtractMetadata(project, runs, extension);
+    }
+
+    private static void createExtractTextures(final @NonNull Project project, final RunManager runs, final BonorumExtension extension)
+    {
         final String name = "exportTextures";
 
         if (runs.findByName(name) != null)
@@ -33,26 +57,49 @@ public class ProjectPlugin implements Plugin<Project> {
 
         final Run clientRun = runs.getByName("client");
         runs.create(name, run -> {
-            run.configure(clientRun);
-            run.getModSources().addAllLater(clientRun.getModSources().all());
-            run.configureFromTypeWithName(false);
-            run.getIDERunName().set("Export textures");
-            run.getExtensions().getByType(IdeaRunExtension.class).getPrimarySourceSet().set(clientRun.getExtensions().getByType(IdeaRunExtension.class).getPrimarySourceSet());
-            run.getWorkingDirectory().set(
-                    project.file("runs/exporters/texture")
-            );
-
-            project.getConfigurations().getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)
-                            .getDependencies().addLater(extension.getSinytraExporterVersion()
-                            .map("org.sinytra:wiki-exporter-neoforge:%s"::formatted)
-                            .map(project.getDependencies()::create));
-
-            run.getSystemProperties()
-                    .put(OUTPUT_PROPERTY, run.getWorkingDirectory().map(directory -> directory.dir("output").getAsFile().getAbsolutePath()));
-            run.getSystemProperties()
-                    .put(CONFIG_PROPERTY, extension.getConfigFile().map(RegularFile::getAsFile).map(File::getAbsolutePath));
-            run.getSystemProperties()
-                    .put(ENABLED_PROPERTY, "true");
+            configureRun(project, extension, run, clientRun, "texture", "Export textures");
         });
+    }
+
+    private static void createExtractMetadata(final @NonNull Project project, final RunManager runs, final BonorumExtension extension)
+    {
+        final String name = "exportMetadata";
+
+        if (runs.findByName(name) != null)
+            return;
+
+        final Run clientRun = runs.getByName("server");
+        runs.create(name, run -> {
+            configureRun(project, extension, run, clientRun, "metadata", "Export metadata");
+
+            run.getArguments().add("--nogui");
+        });
+    }
+
+    private static void configureRun(
+        final @NonNull Project project,
+        final BonorumExtension extension,
+        final Run run,
+        final Run templateRun,
+        final String typeName,
+        final String diplayName)
+    {
+        run.configure(templateRun);
+        run.getModSources().addAllLater(templateRun.getModSources().all());
+        run.configureFromTypeWithName(false);
+        run.getIDERunName().set(diplayName);
+        run.getExtensions().getByType(IdeaRunExtension.class).getPrimarySourceSet().set(templateRun.getExtensions().getByType(IdeaRunExtension.class).getPrimarySourceSet());
+        run.getWorkingDirectory().set(
+                project.file("runs/exporters/%s".formatted(
+                    typeName
+                ))
+        );
+
+        run.getSystemProperties()
+                .put(OUTPUT_PROPERTY, run.getWorkingDirectory().map(directory -> directory.dir("output").getAsFile().getAbsolutePath()));
+        run.getSystemProperties()
+                .put(CONFIG_PROPERTY, extension.getConfigFile().map(RegularFile::getAsFile).map(File::getAbsolutePath));
+        run.getSystemProperties()
+                .put(ENABLED_PROPERTY, "true");
     }
 }
